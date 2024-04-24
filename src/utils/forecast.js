83db532a7cd1df4cbc4d971357d7eba38d2e520f
leoesk <157589545+leoesk@users.noreply.github.com>
2024-04-24 22:05:36 +0200

    dev start

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,10 @@ const forecast = (latitude, longitude, callback) => {
                 } else if (body.error) {
                     callback('Unable to find location.', undefined)
                 } else {
-                    callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. If feels like ' + body.current.feelslike + ' degress out.' + ' The humidity is ' + body.current.humidity + '%')
+                    callback(
+                        undefined,
+                        body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. If feels like '+
+                        body.current.feelslike + ' degress out.' + ' The humidity is ' + body.current.humidity + '%')
                 }})
 }
  
